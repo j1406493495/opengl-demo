0f4529fdac294f7b092ae63766d460987afc8856
Woong <1406493495@qq.com>
2019-12-29 16:50:18 +0800

    [Bug XXXX] 修改错误注释 Cause: None Solution: None Fixed Version: None Branch: None

--- app/src/main/res/raw/simple_vertex_shader.glsl
+++ app/src/main/res/raw/simple_vertex_shader.glsl
@@ -1,8 +1,8 @@
-attribute vec4 a_Position;
+attribute vec4 a_Position; // attribute 修饰符只能在顶点着色器使用，一般用来表示点数据
 //attribute vec4 a_Color;
-//varying vec4 v_Color; varying 属性用来混合颜色
+//varying vec4 v_Color; varying 修饰符表示值可以传递到片段着色器
 // 利用该矩阵将虚拟坐标空间转换为归一化坐标
-uniform mat4 u_Matrix;
+uniform mat4 u_Matrix;  // uniform 修饰符类似 const，表示不会被改变的数据
 
 void main()
 {
