c87e2961f7d91ffcc1be39c4ec6009bf527b6727
Woong <1406493495@qq.com>
2019-12-29 21:04:35 +0800

    [Bug XXXX] 纹理使用注释 Cause: None Solution: None Fixed Version: None Branch: None

--- app/src/main/java/cn/woong/opengl/utils/TextureHelper.kt
+++ app/src/main/java/cn/woong/opengl/utils/TextureHelper.kt
@@ -11,6 +11,7 @@ object TextureHelper {
      * 加载 Android 资源并转化为纹理资源
      */
     fun loadTexture(context: Context, resourceId: Int): Int {
+        // 生成纹理 ID
         val textureObjectIds = IntArray(1)
         GLES20.glGenTextures(1, textureObjectIds, 0)
 
@@ -29,12 +30,17 @@ object TextureHelper {
             return 0
         }
 
+        // 绑定纹理 ID 为二维纹理
         GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, textureObjectIds[0])
+        // 纹理放大缩小规则
         GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MIN_FILTER, GLES20.GL_LINEAR_MIPMAP_LINEAR)
         GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MAG_FILTER, GLES20.GL_LINEAR)
+        // 加载 bitmap 到 openGl 环境
         GLUtils.texImage2D(GLES20.GL_TEXTURE_2D, 0, bitmap, 0)
         bitmap.recycle()
+        // 创建 mip 贴图
         GLES20.glGenerateMipmap(GLES20.GL_TEXTURE_2D)
+        // 解绑纹理 ID
         GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, 0)
 
         return textureObjectIds[0]
